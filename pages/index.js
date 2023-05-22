cc1eb21c2d59762e843266de9eb45ff15518c8f0
Pedro Chaves <draygom@gmail.com>
2023-05-22 19:05:25 +0000

    configura o analytics na pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+
 function Home() {
-    return <h1>Se vc ta vendo isso da um joinha</h1>
+    return (
+        <>
+          <h1>Se vc ta vendo isso da um joinha</h1>
+          <Analytics />
+        </>
+    )
 }
 
 export default Home;
