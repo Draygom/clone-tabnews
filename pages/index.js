dd662257bcac6f6513ed951282726885a39a2ef0
Pedro Chaves <draygom@gmail.com>
2023-06-24 01:06:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 function Home() {
-    return (
-        <>
-          <h1>Se vc ta vendo isso da um joinha</h1>
-          <Analytics />
-        </>
-    )
+  return (
+    <>
+      <h1>Se vc ta vendo isso da um joinha</h1>
+      <Analytics />
+    </>
+  );
 }
 
 export default Home;
