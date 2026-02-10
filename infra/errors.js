48b5bfeba6b9ac51fd3de6449964fd21c340914c
Pedro A Chaves <15214389+Draygom@users.noreply.github.com>
2026-02-10 00:25:33 +0000

    feat: create and use 	 ValidationError`in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -23,9 +23,29 @@ export class ServiceError extends Error {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
-    this.name = "ServiceError ";
+    this.name = "ServiceError";
     this.action = "Verifique se o serviço está disponível.";
     this.status_code = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.status_code,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.status_code = 400;
   }
 
   toJSON() {
